643a72993ab86276a70b6268697bd61338ae1c0a
pranshubarar1 <pranshubarar1851996@gmail.com>
2024-10-21 22:02:05 +0530

    Implement recursive approach for building query from incoming json filter

--- elasticsearch_component/src/main/java/com/optimization_component/service/ElasticSearchServiceImpl.java
+++ elasticsearch_component/src/main/java/com/optimization_component/service/ElasticSearchServiceImpl.java
@@ -15,6 +15,8 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.optimization_component.enums.Operator.*;
+
 @Service
 @AllArgsConstructor
 public class ElasticSearchServiceImpl<T> implements ElasticSearchService<T> {
@@ -31,153 +33,120 @@ public class ElasticSearchServiceImpl<T> implements ElasticSearchService<T> {
 
     @Override
     public void delete(String indexName, String _id) throws IOException {
-
         elasticClient.delete(d -> d
                 .index(indexName)
                 .id(_id)
         );
     }
-
-
     /*
     Search is incomplete as of now. A discussion is required on how the searches have
     to be performed, on what basis, what parameters etc.
     */
     @Override
     public List<T> search(String indexName, Filter filter) throws Exception {
+        Query query = queryBuilder(filter);
 
-        BoolQuery.Builder boolQuery = new BoolQuery.Builder();
+        var searchResponse = elasticClient.search(s -> s
+                .index(indexName)
+                .query(query),
+                Object.class
+        );
 
-        return List.of();
+        return searchResponse.hits().hits().stream()
+                .map(hit -> (T) hit.source())
+                .toList();
 
     }
 
-    private void queryBuilder(BoolQuery.Builder boolQuery, Filter filter){
-
-
+    private Query queryBuilder(Filter filter) {
         /*
             Below is the structure of Filter class
             public class Filter {
                 private Operator operator;
+                        +
+                //----------------
                 private String field;
                 private Object value;
+                //----------------
+
+                        OR
+
+                //----------------
                 private List<Filter> filters;
+                //----------------
             }
             There can be two cases:
-            Either
+            EITHER
             1) The "field" && "value" both will be null and "filters"
             list will be non-null -> which means we have to go more deep into recursion
             OR
             2) The "filters" field will be null and "field" && "value"
-            both will be non-null -> which means its a leaf filter and its a base-case
+            both will be non-null -> which means it's a leaf filter, and it's a base-case
             and we have to return
 
-            !! Note: Its very important to keep in mind that both:
+            !! Note: It's very important to keep in mind that both:
             => "field" && "value" pair
             and
             => "filters"
             can't neither be "null" nor "non-null" at the same time, as it will be
             ambiguous case
         */
+        BoolQuery.Builder builder = new BoolQuery.Builder();
 
         //So now let's first define base-case:
-        if(filter.getField() != null && filter.getValue() != null && filter.getFilters() == null){
-            //so this is base-case
-
+        if (((filter.getField() != null && filter.getValue() != null) && filter.getFilters() == null)) {
+            return buildQuery(filter);
+        } else if ((filter.getField() == null && filter.getValue() == null) && filter.getFilters().size() == 1) {
+            return builder.must(buildQuery(filter.getFilters().get(0))).build()._toQuery();
         }
 
 
+        //if code reaches this point it means "field" and "value" both are null and "filters' list" is not null &&
+        //its size > 1
+        //So this is the perfect case for recursion
 
+        for(Filter currentFilter : filter.getFilters()){
+            //going deep into recursion:
+            Query query = queryBuilder(currentFilter);
 
+            if(filter.getFilters().size() == 1){
+                return query;
+            }
+            if(filter.getOperator() == AND) {
+                builder.must(query);
+            }
+            else if (filter.getOperator() == OR) {
+                builder.should(query);
+            }
+        }
+        return builder.build()._toQuery();
+    }
 
+    private Query buildQuery(Filter filter) {
+        Object value = filter.getValue();
+        String field;
 
+        if (value instanceof String) {
+            field = filter.getField() + ".keyword";
+        } else {
+            field = filter.getField();
+        }
+        switch (filter.getOperator()) {
+            case EQ:
+                return Query.of(q -> q.term(t -> t.field(field).value((FieldValue.of(value)))));
+
+            case GREATER_THAN:
+                return Query.of(q -> q.range(r -> r.field(field).gt(JsonData.of(value))));
+
+            case LESS_THAN:
+                return Query.of(q -> q.range(r -> r.field(field).lt((JsonData.of(value)))));
+
+            case BETWEEN: {
+                Map<String, Object> map = (LinkedHashMap<String, Object>) value;
+                return Query.of(q -> q.range(r -> r.field(field).gte(JsonData.of(map.get("from"))).lte(JsonData.of(map.get("to")))));
+            }
+            default:
+                throw new UnsupportedOperationException("Unknown comparison operator: " + filter.getOperator());
+        }
     }
 }
-
-
-//
-//
-//
-//        for(int i = 0; i< leafFilters.size(); i++){
-//            LeafFilter leafFilter = leafFilters.get(i);
-//
-//            //We will check the second leafFilter's operator for deciding whether first will
-//            //go into "should" or "must" [BUT THIS WILL BE DONE ONLY FOR FIRST FILTER]
-//            if(i == 0) {
-//                if(leafFilters.size() == 1){
-//                    //it means only one leafFilter is there and this will be added to "must" clause
-//                    addToMustClause(boolQuery, leafFilter);
-//                }
-//                else {
-//                    //extract logicalOperator from second leafFilter
-//                    LogicalOperator logicalOperatorOfSecondFilter = leafFilters.get(1).getLogicalOperator();
-//                    if(logicalOperatorOfSecondFilter.equals(LogicalOperator.OR)){
-//                        //so in this case this first leafFilter will be added to the "should" clause
-//                        addToShouldClause(boolQuery, leafFilter);
-//                    } else {
-//                        //this first leafFilter will be added to the "must" clause
-//                        addToMustClause(boolQuery, leafFilter);
-//                    }
-//                }
-//            } else {
-//                if(leafFilter.getLogicalOperator().equals(LogicalOperator.OR)){
-//                    addToShouldClause(boolQuery, leafFilter);
-//                } else {
-//                    addToMustClause(boolQuery, leafFilter);
-//                }
-//            }
-//
-//        Query query = Query.of(q -> q.bool(boolQuery.build()));
-//
-//        //See now we have the query now I want to send this query to search api of elasticsearch and receive the result
-//        //so how to do that here?
-//        var searchResponse = elasticClient.search(s -> s
-//                .index(indexName)
-//                .query(query),
-//                Object.class
-//        );
-//
-//        return searchResponse.hits().hits().stream()
-//                .map(hit -> (T) hit.source())
-//                .toList();
-//
-//    }
-//
-//    private void addToShouldClause(BoolQuery.Builder boolQuery, LeafFilter leafFilter) {
-//        boolQuery.should(buildQuery(leafFilter));
-//    }
-//
-//    private void addToMustClause(BoolQuery.Builder boolQuery, LeafFilter leafFilter) {
-//        boolQuery.must(buildQuery(leafFilter));
-//    }
-//
-//    private Query buildQuery(LeafFilter leafFilter) {
-//
-//
-//        Object value = leafFilter.getValue();
-//        String field;
-//
-//        if(value instanceof String){
-//            field = leafFilter.getField() + ".keyword";
-//        } else {
-//            field = leafFilter.getField();
-//        }
-//
-//        switch (leafFilter.getComparisonOperator()) {
-//            case EQUALS:
-//                return Query.of(q -> q.term(t -> t.field(field).value((FieldValue.of(value)))));
-//
-//            case GREATER_THAN:
-//                return Query.of(q -> q.range(r -> r.field(field).gt(JsonData.of(value))));
-//
-//            case LESS_THAN:
-//                return Query.of(q -> q.range(r -> r.field(field).lt((JsonData.of(value)))));
-//
-//            case BETWEEN: {
-//                Map<String,Object> map = (LinkedHashMap<String, Object>) value;
-//                return Query.of(q -> q.range(r -> r.field(field).gte(JsonData.of(map.get("from"))).lte(JsonData.of(map.get("to")))));
-//            }
-//            default:
-//                    throw new UnsupportedOperationException("Unknown comparison operator: " + leafFilter.getComparisonOperator());
-//        }
-//    }
